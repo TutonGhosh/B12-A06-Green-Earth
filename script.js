046f436d34cd3a352df906142f1a8408b87386d0
TutonGhosh <404ghosh@gmail.com>
2026-06-06 12:57:35 +0600

    categories button fucntion added

--- script.js
+++ script.js
@@ -1,3 +1,60 @@
+const removeActiveClass = () => {
+    const buttons = document.querySelectorAll(".category-btn");
+
+    buttons.forEach(btn => {
+        btn.classList.remove("active-btn");
+    });
+};
+const setActiveClass = (btn) => {
+    removeActiveClass();
+    btn.classList.add("active-btn");
+};
+
+// id	1
+// image	"https://i.ibb.co.com/cSQdg7tf/mango-min.jpg"
+// name	"Mango Tree"
+// description	"A fast-growing tropical tree that produces delicious, juicy mangoes during summer. Its dense green canopy offers shade, while its sweet fruits are rich in vitamins and minerals."
+// category	"Fruit Tree"
+// price	500
+const loadCatTree = (id, btn) => {
+    setActiveClass(btn);
+
+    fetch(`https://openapi.programming-hero.com/api/category/${id}`)
+    .then((resp) => resp.json())
+    .then((data) => displayTreeCatWise(data.plants))
+}
+displayTreeCatWise = (data) => {
+
+    const allCatCards = document.getElementById("all-cat-cards");
+    allCatCards.innerHTML = "";
+
+    data.forEach(item => {
+        // console.log(item.name)
+
+        const card = document.createElement("div");
+        card.innerHTML = `
+        <div class="card bg-base-100 max-w-96 shadow-sm rounded-xl">
+                            <figure class="p-5 pb-0">
+                                <img class="rounded-xl h-[250px] w-full object-cover object-center" src="${item.image}" alt="Shoes" />
+                            </figure>
+                            <div class="card-body space-y-1">
+                                <h2 class="card-title">${item.name}</h2>
+                                <p>${item.description}</p>
+                                <div class="flex items-center justify-between px-2">
+                                    <button class="btn btn-soft btn-success border-none shadow-none">${item.category}</button>
+                                    <h1 class = "text-xl font-semibold">৳ ${item.price}</h1>
+                                </div>
+                                <div class="card-actions">
+                                    <button class="btn btn-ghost text-[#15803d] hover:bg-[#15803d] hover:text-white font-medium border-[#15803d] shadow-none rounded-full w-full">Add to Cart</button>
+                                </div>
+                            </div>
+                        </div>
+        `;
+        
+        allCatCards.appendChild(card);
+    })
+}
+
 const loadAllBtn = () => {
   fetch("https://openapi.programming-hero.com/api/categories")
     .then((resp) => resp.json())
@@ -10,15 +67,15 @@ const displayAllBtn = (data) => {
 
     const allCatBtn = document.getElementById("all-cat-btn");
     allCatBtn.innerHTML = `
-    <button class="btn btn-ghost bg-[#15803d] text-white font-medium border-none shadow-none">All Categories</button>
+    <button onclick = "loadAllCard(this), setActiveClass(this)" class="btn btn-ghost font-medium border-none shadow-none category-btn active-btn">All Categories</button>
     `;
 
     data.forEach(item => {
-        console.log(item.category_name)
+        // console.log(item.category_name)
 
         const catBtn = document.createElement("div");
         catBtn.innerHTML = `
-        <button onclick = "loadCatTree(${item.id})" class="btn btn-ghost hover:bg-[#15803d] hover:text-white font-medium border-none shadow-none">${item.category_name}</button>
+        <button onclick = "loadCatTree(${item.id}, this)" class="btn btn-ghost hover:bg-[#15803d] hover:text-white font-medium border-none shadow-none category-btn">${item.category_name}</button>
         `;
 
         allCatBtn.appendChild(catBtn);
@@ -42,7 +99,7 @@ const displayAllCard = (data) => {
     allCatCards.innerHTML = "";
 
     data.forEach(item => {
-        console.log(item.name)
+        // console.log(item.name)
 
         const card = document.createElement("div");
         card.innerHTML = `
@@ -100,101 +157,3 @@ const loadCart = () => {
 loadCart()
 loadAllCard()
 loadAllBtn();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const loadAllTrees = () => {
-//   fetch("https://openapi.programming-hero.com/api/categories")
-//     .then((resp) => resp.json())
-//     .then((json) => displayAllCatBtn(json.categories));
-// };
-// // {
-// // "id": 1,
-// // "category_name": "Fruit Tree",
-// // "small_description": "Trees that bear edible fruits like mango, guava, and jackfruit."
-// // }
-// const displayAllCatBtn = (allCat) => {
-
-//     const allCatBtn = document.getElementById("all-cat-btn");
-//     allCatBtn.innerHTML = `
-//     <h1 class="mb-5 text-xl font-semibold">Categories</h1>
-//     <button class="btn text-white font-medium bg-[#15803d] border-none shadow-none">All Categories</button>
-//     `;
-
-//     const allCards = document.getElementById("all-cards");
-//     allCards.innerHTML = "";
-
-//     allCat.forEach(cat => {
-//         const catBtn = document.createElement("div");
-//         catBtn.innerHTML = `
-//         <button onclick = "loadCatTree(${cat.id})" class="btn btn-ghost font-medium border-none shadow-none">${cat.category_name}</button>
-//         `;
-
-//         const allCategoriesCard = document.createElement("div");
-//         allCategoriesCard.innerHTML = `
-//         <div class="card bg-base-100 w-96 shadow-sm rounded-xl">
-//                             <figure class="p-5">
-//                                 <img class="rounded-xl" src="${cat}" alt="Shoes" />
-//                             </figure>
-//                             <div class="card-body space-y-2">
-//                                 <h2 class="card-title">Card Title</h2>
-//                                 <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-//                                 <div class="flex items-center justify-between px-2">
-//                                     <button class="btn btn-soft btn-success border-none shadow-none">fruit tree</button>
-//                                     <h1>$1500</h1>
-//                                 </div>
-//                                 <div class="card-actions">
-//                                     <button class="btn text-white font-medium bg-[#15803d] border-none shadow-none rounded-full w-full">Add to Cart</button>
-//                                 </div>
-//                             </div>
-//                         </div>
-//         `;
-
-//         allCatBtn.appendChild(catBtn)
-//         allCards.appendChild(allCategoriesCard);
-//     });
-// }
-
-// loadAllTrees();
